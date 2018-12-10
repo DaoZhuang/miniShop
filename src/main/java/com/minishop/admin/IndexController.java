987514ae79296cf2301d64bccd3b8872d934a72c
Zack <237598959@qq.com>
2018-12-10 17:30:01 +0800

    构建项目 提交

--- src/main/java/com/minishop/admin/IndexController.java
+++ src/main/java/com/minishop/admin/IndexController.java
@@ -1,4 +1,26 @@
 package com.minishop.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("index")
 public class IndexController {
+
+    @RequestMapping("test")
+    public ModelAndView index(){
+        System.out.println("请求成功！！！");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+
+    @RequestMapping("test2")
+    public ModelAndView index2(){
+        System.out.println("请求成功！！！");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index2");
+        return mv;
+    }
 }
